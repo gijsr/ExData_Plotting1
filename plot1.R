6475a14c8f3b63963b1df9cc4999cff8360c7a06
GijsR <gijsbertus@me.com>
2015-02-05 11:05:14 +0100

    Small typographical changes

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-# plot 1.R
+# plot1.R
 
 # Part 1 Download, unzip, read, subset and convert data
 fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"  #URL of data source
@@ -11,7 +11,6 @@ projectfile$Date<-as.Date(projectfile$Date, format="%d/%m/%Y")  # convert Date r
 projectfile$DateTime <- as.POSIXct(paste(projectfile$Date, projectfile$Time))  # concatenate date and time cols, convert to POSIX and put in new column
 
 # Part 2 Generate .png file with plot
-
 png(file = "plot1.png", width=480, height=480, bg="transparent")
 hist(projectfile$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
 dev.off()
